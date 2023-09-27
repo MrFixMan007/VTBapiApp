a7bd6103e0a6717c53c24ff6f226172e5e68a10b
MrFixMan007 <schurkin.danil@yandex.ru>
2023-09-27 15:32:13 +0400

    Инициализация репозитория

---
+++ app/src/main/java/com/example/vtbapiapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.vtbapiapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
