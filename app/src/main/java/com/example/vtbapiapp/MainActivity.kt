8efb62ec0607036182d3699cae39ef076b992f10
HanzerI <forvpinj@gmail.com>
2023-10-15 06:18:30 +0400

    Настройки работы с базой данных

--- app/src/main/java/com/example/vtbapiapp/MainActivity.kt
+++ app/src/main/java/com/example/vtbapiapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.vtbapiapp
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
+import android.location.Geocoder
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
